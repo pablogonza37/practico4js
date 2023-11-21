a11d03a6f0a75d7de43f85d2d382a214a18a98ac
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-11-21 01:16:52 -0300

    se realizo ejercicio 8 tp4

---
+++ js/tp4-ejercicio8.js
@@ -0,0 +1,53 @@
+class Persona {
+    constructor(nombre, edad, profesion) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.profesion = profesion;
+    }
+
+    get nombre() {
+        return this._nombre;
+      }
+    
+      get edad() {
+        return this._edad;
+      }
+    
+      get profesion() {
+        return this._profesion;
+      }
+    
+      set nombre(nuevoNombre) {
+        this._nombre = nuevoNombre;
+      }
+    
+      set edad(nuevaEdad) {
+        if (nuevaEdad > 0) {
+          this._edad = nuevaEdad;
+        } else {
+          console.error("La edad debe ser un número positivo.");
+        }
+      }
+    
+      set profesion(nuevaProfesion) {
+        this._profesion = nuevaProfesion;
+      }
+  
+    saludar() {
+      document.write(`<p>Hola, soy ${this.nombre}, tengo ${this.edad} años y soy ${this.profesion}.</p>`);
+    }
+  
+    despedirse() {
+      document.write(`<p>Adiós, Hasta luego.</p>`);
+    }
+  }
+  
+  const persona1 = new Persona('Rodrigo', 30, 'Programador');
+  const persona2 = new Persona('Evelin', 25, 'Psicologa');
+  
+  persona1.saludar();
+  persona1.despedirse();
+  
+  persona2.saludar();
+  persona2.despedirse();
+  
